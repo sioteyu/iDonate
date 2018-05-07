346813394a6674c019c37bcac3ef941f451803fd
anrydomingo <anrydomingo@gmail.com>
2018-05-08 00:54:17 +0800

    camera feature added, unfinished

---
+++ app/src/main/java/com/jograt/atenatics/idonate/SellFrgament.java
@@ -0,0 +1,62 @@
+package com.jograt.atenatics.idonate;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class SellFrgament extends Fragment {
+
+    private ImageView imageView;
+    private EditText editName;
+    private EditText editDescription;
+    private Button btnOK;
+    private Button btnCancel;
+
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sell, container, false);
+
+        imageView = view.findViewById(R.id.itemImage);
+        editName = view.findViewById(R.id.editTextName);
+        editDescription = view.findViewById(R.id.editTextDescription);
+        btnOK = view.findViewById(R.id.btnPost);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+
+        }
+
+        Intent cameraIntent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == getActivity().RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+
+    }
+
+}
