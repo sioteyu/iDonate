18f7c75a4fdc60fd87ef833faa4c431e23372d85
Greg Cabrera <gregcabrera422@gmail.com>
2018-05-08 10:56:22 +0800

    added camera permission

--- app/src/main/java/com/jograt/atenatics/idonate/MainActivity.java
+++ app/src/main/java/com/jograt/atenatics/idonate/MainActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Gravity;
 import android.support.design.widget.NavigationView;
@@ -72,49 +74,6 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    private boolean hasCameraPermission(){
-        int result = 0;
-
-        String[] permissions = new String[]{Manifest.permission.CAMERA};
-
-        for(String permission: permissions){
-            result = checkCallingOrSelfPermission(permission);
-
-            if(result != PackageManager.PERMISSION_GRANTED){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void requestCameraPermission(){
-        String[] permissions = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION};
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            requestPermissions(permissions, 1);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        switch (requestCode){
-            case 1:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    /*//run intent
-                    Intent intent = new Intent(LoginActivity.this, WeatherFragment.class);
-                    startActivity(intent);*/
-                }else {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        if (shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
-                            Toast.makeText(this, "Camera Access Denied!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-                break;
-        }
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -123,11 +82,14 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_donate) {
 
-            if(hasCameraPermission()){
+            if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CAMERA}, 1);
+
                 DonateFragment fragment = new DonateFragment();
                 manager.beginTransaction().replace(R.id.include, fragment).addToBackStack(null).commit();
             }else{
-                requestCameraPermission();
+                DonateFragment fragment = new DonateFragment();
+                manager.beginTransaction().replace(R.id.include, fragment).addToBackStack(null).commit();
             }
 
         } else if (id == R.id.nav_profile) {
@@ -170,6 +132,49 @@ public class MainActivity extends AppCompatActivity
                 break;
             case MaterialSearchBar.BUTTON_BACK:
                 searchBar.disableSearch();
+                break;
+        }
+    }
+
+    private boolean hasCameraPermission(){
+        int result = 0;
+
+        String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        for(String permission: permissions){
+            result = checkCallingOrSelfPermission(permission);
+
+            if(result != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void requestCameraPermission(){
+        String[] permissions = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(permissions, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    /*//run intent
+                    Intent intent = new Intent(LoginActivity.this, WeatherFragment.class);
+                    startActivity(intent);*/
+                }else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                            Toast.makeText(this, "Camera Access Denied!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
                 break;
         }
     }
